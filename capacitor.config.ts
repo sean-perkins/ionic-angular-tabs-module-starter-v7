2f2cc1c73a5893c2e9bb4ff0a00e8593bd126096
Sean Perkins <sean@ionic.io>
2023-03-31 12:02:25 -0400

    fix: update project name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-angular-swipe-segment',
+  appName: 'ionic-angular-tabs',
   webDir: 'www',
   bundledWebRuntime: false
 };
